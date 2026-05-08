9d492fad0d1d139a2a9b1aed68876decfcdc9aa6
webexpert9876 <124126909+webexpert9876@users.noreply.github.com>
2026-05-08 11:26:12 +0530

    Fix indentation in About component

--- pages/about.js
+++ pages/about.js
@@ -6,7 +6,7 @@ const URL =process.env.STRAPIBASEURL;
 const About = () => {
   return (
     <div>
-      <div className="container mx-auto max-w-7xl px-2 sm:px-6 lg:px-8"> 
+      <div className="container mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
       About
       </div>
     </div>
